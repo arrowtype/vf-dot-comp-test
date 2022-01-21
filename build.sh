cf3c842bb0f53768e31688c7c771071e9ffdf00d
arrowtype <arrowtypeco@gmail.com>
2022-01-20 20:26:23 -0500

    fix /exclam incompatibility by removing dot scaling

---
+++ build.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+# build VF with supports
+fontmake -m source/Dot_Comp_Test-wght_1_1000-opsz_12_96.designspace -o variable
+
+# build VF with supports
+fontmake -m source/Dot_Comp_Test-wght_1_1000-opsz_12_96--w_sparse_supports.designspace -o variable
