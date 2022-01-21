030b3d5c2e6ffaa1862e57e0946f22e587b0e5c2
arrowtype <arrowtypeco@gmail.com>
2022-01-21 12:38:34 -0500

    add test with CCW paths

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 # !/bin/bash
 
 # build VF with supports
-fontmake -m source/Dot_Comp_Test-wght_1_1000-opsz_12_96--w_sparse_supports.designspace -o variable  --output-path fonts/full-designspace/Dot_Comp_Test-VF.ttf
+fontmake -m source/clockwise-paths/Dot_Comp_Test-wght_1_1000-opsz_12_96--w_sparse_supports.designspace -o variable  --output-path fonts/full-designspace/Dot_Comp_Test-VF.ttf
 
 # build VF with NO supports
-fontmake -m source/Dot_Comp_Test-wght_1_1000-opsz_12_96.designspace -o variable --output-path fonts/no-supports/Dot_Comp_Test-no_supports-VF.ttf
+fontmake -m source/clockwise-paths/Dot_Comp_Test-wght_1_1000-opsz_12_96.designspace -o variable --output-path fonts/no-supports/Dot_Comp_Test-no_supports-VF.ttf
 
 # build statics with supports
-fontmake -m source/Dot_Comp_Test-wght_1_1000-opsz_12_96--w_sparse_supports.designspace -o ttf -i  --output-dir fonts/full-designspace/static --expand-features-to-instances
+fontmake -m source/clockwise-paths/Dot_Comp_Test-wght_1_1000-opsz_12_96--w_sparse_supports.designspace -o ttf -i  --output-dir fonts/full-designspace/static --expand-features-to-instances
 
 # build static with NO supports
-fontmake -m source/Dot_Comp_Test-wght_1_1000-opsz_12_96.designspace -o ttf -i --output-dir fonts/no-supports/static --expand-features-to-instances
+fontmake -m source/clockwise-paths/Dot_Comp_Test-wght_1_1000-opsz_12_96.designspace -o ttf -i --output-dir fonts/no-supports/static --expand-features-to-instances
+
+# build VF with supports, CCW paths
+fontmake -m source/counter_clockwise-paths/Dot_Comp_Test-wght_1_1000-opsz_12_96--w_sparse_supports.designspace -o variable  --output-path fonts/counter_clockwise-paths/Dot_Comp_Test-CCW-VF.ttf
+
+# build statics with supports, CCW paths
+fontmake -m source/counter_clockwise-paths/Dot_Comp_Test-wght_1_1000-opsz_12_96--w_sparse_supports.designspace -o ttf -i  --output-dir fonts/counter_clockwise-paths/static --expand-features-to-instances
+
 
 # copy fonts to /docs for GitHub Pages
-cp -r fonts docs/fonts
+cp -r fonts docs
